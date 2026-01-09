7ca99d918c15653adeb84b5f38aabaa62804a052
mwalegacybuiltco-ops <mwalegacybuiltco@gmail.com>
2026-01-08 20:58:35 -0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ window.CHB_CONFIG = {
   // AFFILIATE BACK OFFICE (hidden unless unlocked)
   affiliateBackOffice: {
     startHere: "https://docs.google.com/document/d/1aVUoTo7Imo2srT53p1fKK7CaPSJXeik_3oM1tBg9duU/edit?usp=sharing",
-    training: "https://beacons.ai/PASTE_TRAINING",
+    training: "https://docs.google.com/document/d/1yTLrnvWvK7MEjhdCEV8dVRqYWY3FZT20bWYPFQgr-Lk/edit?usp=sharing",
     fourCorners: "https://docs.google.com/document/d/13FA9E7lfVdolYj_yexjfuY6Wy7FM2tNGBjTckrHrR0I/edit?usp=sharing",
     teamBuilder: "https://mwalegacybuiltco-ops.github.io/MWA-LegacyBuilt-OS-3.2.9/",
     payouts: "https://forms.gle/u1Ag7QFY3UZBFh4J9",
