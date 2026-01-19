ec440fe1a9b4f0bb339da214e0b47b1c3a4f6aea
mwalegacybuiltco-ops <mwalegacybuiltco@gmail.com>
2026-01-19 14:27:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ window.CHB_CONFIG = {
     },
     about: {
       ourStory: "https://docs.google.com/document/d/1VOtr23jMYWVdlcWUiQ_Sup4AH6-HQnadODGU-lSPJwg/edit?usp=sharing",
-      community: "https://www.facebook.com/share/g/1C14TwFpFL/?mibextid=wwXIfr"
+      community: "https://www.facebook.com/share/g/1AFDJocmrZ/"
     },
     workWithUs: {
       // This is your PUBLIC "Share & Earn" page or Google Form link
